6cd91bf5b0ab8eaa1cb978f0a89a1451449266e6
corbin <stramapatryk@gmail.com>
2023-08-30 21:32:54 +0200

    Added scene class to get a view of bubbles. Completed resizeEvent in Window to resize view to size of window.

--- scene/scene.py
+++ scene/scene.py
@@ -1,6 +1,24 @@
+from PyQt6.QtWidgets import QGraphicsView, QGraphicsScene
+from PyQt6.QtGui import QResizeEvent
+from bubble import Bubble
 
 
-class Scene:
+class Scene(QGraphicsView):
 
-    def __init__(self):
+    def __init__(self, parent,  width, height):
+        self.plane = QGraphicsScene(0, 0, width, height)
+        super().__init__(self.plane)
+        self.setParent(parent)
+        # Bubble.set_scene(self.plane)
+
+        # self.bubble = Bubble()
+
+    def mousePressEvent(self, event) -> None:
         pass
+
+    def mouseReleaseEvent(self, event) -> None:
+        pass
+
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        size = event.size()
+        self.plane.setSceneRect(0, 0, size.width(), size.height())
--- window.py
+++ window.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QLabel, QMainWindow, QApplication
 from PyQt6.QtGui import *
 import sys
+from scene import Scene
 
 
 class Window(QMainWindow):
@@ -12,11 +13,17 @@ class Window(QMainWindow):
         super().__init__()
         self.setWindowTitle("Bubbles")
         self.setWindowIcon(QIcon('images/icon.png'))
-        self.setFixedSize(1280, 960)
+        width = 640
+        height = 640
+        self.setMinimumSize(width, height)
         self.background = QLabel()
         self.setCentralWidget(self.background)
+
+        self.scene = Scene(self, width, height)
+
         self.show()
         sys.exit(self.app.exec())
 
     def resizeEvent(self, event: QResizeEvent):
         super().resizeEvent(event)
+        self.scene.resize(event.size())
