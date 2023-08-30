3d18071a3d1e87b3250dfe1460f8d13bc248dac1
corbin <stramapatryk@gmail.com>
2023-08-30 23:40:05 +0200

    BubbleCreator now can create Bubble that grow by time in create function. stop_grow delete timer that grows the bubble.

--- bubble/bubble_creator.py
+++ bubble/bubble_creator.py
@@ -1,15 +1,23 @@
+from PyQt6.QtCore import QTimer, QPoint
+from bubble.bubble import Bubble
 
 
 class BubbleCreator:
 
     def __init__(self):
-        pass
 
-    def clear(self):
-        pass
+        self.grow_value = 1
+        self.timer = None
+        self.bubble = None
 
-    def grow(self):
-        pass
+    def create(self, position: QPoint):
+        if self.bubble is None:
+            self.bubble = Bubble(position.x(), position.y())
+            self.timer = QTimer()
+            self.timer.timeout.connect(self.bubble.grow)
+            self.timer.start(100)
 
-    def get(self):
-        pass
+    def stop_grow(self):
+        if self.bubble is not None:
+            del self.timer
+            self.bubble = None
