435e47b1b03701d4fd81e42f20ac11e6814af225
corbin <stramapatryk@gmail.com>
2023-09-01 22:49:32 +0200

    Added methode physics in Bubble. Using it Bubble float to top. If it moves and it collide it remove other object

--- bubble/bubble.py
+++ bubble/bubble.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QGraphicsPixmapItem, QGraphicsScene
 from PyQt6.QtGui import QPixmap, QImage
-import time
+from PyQt6.QtCore import QPointF
+from physics import PhysicsTimer
 
 
 class Bubble(QGraphicsPixmapItem):
@@ -9,6 +10,7 @@ class Bubble(QGraphicsPixmapItem):
     @classmethod
     def set_scene(cls, scene: QGraphicsScene):
         cls.__scene = scene
+        cls.updater = PhysicsTimer()
 
     def __init__(self, from_left: int = 0, from_top: int = 0):
         self.image = QPixmap(QImage("images/bubble.png"))
@@ -20,6 +22,11 @@ class Bubble(QGraphicsPixmapItem):
         self.__scene.addItem(self)
         self.timer = 0
         self.increment = 4
+
+        self.updater.add_function(self.physics)
+
+    def __del__(self):
+        self.updater.remove_function(self.physics)
 
     def mousePressEvent(self, event) -> None:
         super().mouseReleaseEvent(event)
@@ -38,5 +45,12 @@ class Bubble(QGraphicsPixmapItem):
         self.update_scale()
         self.moveBy(-self.increment/2, -self.increment/2)
 
-    def float(self):
-        pass
+    def physics(self):
+        movement = QPointF(0, -1)
+        pos = self.pos()
+        if pos.y() + movement.y() < 0:
+            movement.setY(0 - pos.y())
+
+        self.moveBy(movement.x(), movement.y())
+        for item in self.collidingItems():
+            self.__scene.removeItem(item)
